a1f65dfc9dbcb0b4aa131eae18838fe9ebc251cf
FloThePro <florian.uhl1998@web.de>
2022-02-27 18:57:48 +0100

    Added intro function in Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Hotelreservierung
+{
+    class Program
+    {
+        static void Main()
+        {
+            string auswahl;
+            Console.WriteLine("Willkommen zum Hausarbeitsprojekt von Florian Uhl!");
+            auswahl = Intro();
+        }
+
+        public static string Intro()
+        {
+            Console.WriteLine("Bitte wählen Sie aus folgenden Aktionen aus:");
+            Console.WriteLine("Gast | Zimmer | Zimmerkategorie | Reservierung");
+            var temp = Console.ReadLine();
+            if (temp == "Reservierung")
+            {
+                Console.WriteLine("Bitte geben Sie an, welche Aktion Sie durchführen wollen.");
+                Console.WriteLine("anlegen | bearbeiten | löschen | zuordnen | ausgeben");
+                temp += Console.ReadLine();
+            }
+            else if (temp == "Zimmer" || temp == "Zimmerkategorie" || temp == "Gast")
+            {
+                Console.WriteLine("Bitte geben Sie an, welche Aktion Sie durchführen wollen.");
+                Console.WriteLine("anlegen | bearbeiten | löschen | ausgeben");
+                temp += Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Ihre Eingabe ist ungültig. Bitte versuchen Sie es noch einmal.");
+            }
+            Console.WriteLine("Bitte geben Sie an, welche Aktion Sie durchführen wollen.");
+            Console.WriteLine("anlegen | bearbeiten | löschen | ausgeben");
+            temp += Console.ReadLine();
+            return temp;
+        }
+    }
+}
